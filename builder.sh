home-assistant/builder

commit 9ed32742b14a903d1c0762dedc5fe6ae27cc332e

Lowercase repository and image (#114)
* Lowercase repository and image

* Fix SC2184

builder.sh
@@ -223,8 +223,8 @@ function run_build() {
     local release="${version}"
 
     # Overwrites
-    if bashio::var.has_value "${DOCKER_HUB}"; then repository="${DOCKER_HUB}"; fi
-    if bashio::var.has_value "${IMAGE}"; then image="${IMAGE}"; fi
+    if bashio::var.has_value "${DOCKER_HUB}"; then repository="${DOCKER_HUB@L}"; fi
+    if bashio::var.has_value "${IMAGE}"; then image="${IMAGE@L}"; fi
     if bashio::var.has_value "${RELEASE}"; then release="${RELEASE}"; fi
 
     # Replace {arch} with build arch for image
@@ -687,7 +687,7 @@ function extract_machine_build() {
         done
 
         for i in "${remove[@]}"; do
-            unset BUILD_MACHINE["$i"]
+            unset "${BUILD_MACHINE["$i"]}"
         done
     fi
 }

commit 2595010b3577ac5bb5787a1494a3dc00a01919dd

Fix machine extraction broken by #114

builder.sh
@@ -701,7 +701,7 @@ function extract_machine_build() {
         done
 
         for i in "${remove[@]}"; do
-            unset "${BUILD_MACHINE["$i"]}"
+            unset $BUILD_MACHINE["$i"]
         done
     fi
 }